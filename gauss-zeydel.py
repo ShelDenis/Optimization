2123c0eea35521f0f2956be7a7ffd44817dda1c9
ShelDenis <shele1-2010@mail.ru>
2025-03-30 13:46:42 +0600

    gauss-zeydel

---
+++ gauss-zeydel.py
@@ -0,0 +1,41 @@
+import numpy as np
+import sympy as sp
+
+
+str_f = '4 * x1 ** 2 + (x2 - 2) ** 2'
+
+
+def f(x1, x2):
+    return 4 * x1 ** 2 + (x2 - 2) ** 2
+
+
+def norma(v):
+    sq_sum = 0
+    for xi in v:
+        sq_sum += xi ** 2
+    return sq_sum ** 0.5
+
+
+eps = 0.01
+x1, x2, a = sp.symbols('x1 x2 a')
+x = [2, 2]
+count = 0
+while norma(gradient(f, x)) >= eps:
+    for i in range(len(x)):
+        s = [1 if i == count else 0 for i in range(len(x))]
+        x_with_step = []
+        str_eq = str_f
+        for i in range(len(x)):
+            elem = str(sp.sympify(eval(f'{x[i]} + a * {s[i]}')))
+            x_with_step.append(sp.sympify(eval(elem)))
+            str_eq = str_eq.replace(f'x{i + 1}', f'({elem})')
+        equation = sp.Eq(sp.sympify(str_eq), 0)
+        alpha = sp.solve(equation, a)
+        x = np.array(x) + alpha * np.array(s)
+        count += 1
+        count %= 2
+
+print(f(x[0], x[1]))
+
+
+
