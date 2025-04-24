eed8af47ab7ab402ff1c8b63b3224ca4e85fe080
ShelDenis <shele1-2010@mail.ru>
2025-04-24 17:41:56 +0600

    additive convolution of criteria method

---
+++ additive_convolution_of_criteria.py
@@ -0,0 +1,47 @@
+import sympy as sp
+import numpy as np
+from scipy.optimize import minimize
+import random
+
+x1, x2 = sp.symbols('x1 x2')
+
+fs = [2 * x1 ** 2 + 5 * x2 ** 2,
+      10 * (x1 - 5) ** 2 + 3 * (x2 + 2) ** 2,
+      7 * (x1 + 2) ** 2 + (x2 - 5) ** 2]
+
+n_weights_changes = 10
+
+best_x, best_f, best_weights = [], float('inf'), []
+
+for k in range(n_weights_changes):
+    crit_f = 0
+    sum_weight = 1
+    weights = []
+    for f in fs:
+        if len(weights) == len(fs) - 1:
+            w = 1 - sum(weights)
+        else:
+            w = random.uniform(0, sum_weight)
+        weights.append(w)
+        crit_f += w * f
+        sum_weight -= w
+
+    func = sp.lambdify((x1, x2), crit_f, 'numpy')
+    x0 = np.array([random.uniform(-10, 10), random.uniform(-10, 10)])
+
+    result = minimize(lambda args: func(*args), x0)
+
+    print(f'Итерация - {k}')
+    print(f'Веса: {weights}')
+    print(f'x* = {result.x}')
+    print(f'f* = {result.fun}')
+
+    if result.fun < best_f:
+        best_x = result.x
+        best_f = result.fun
+        best_weights = weights[:]
+
+print(f"\nЛучшее решение:")
+print(f'Веса: {best_weights}')
+print(f'x* = {best_x}')
+print(f'f* = {best_f}')
